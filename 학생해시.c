fd7431de07a9584177f11acb48c5dd84a565db17
davidpark300 <davidpark300@naver.com>
2024-11-27 23:45:59 +0900

    블랙리스트 관리 마무리

--- 학생해시.c
+++ 학생해시.c
@@ -4,15 +4,14 @@
 #include <string.h>
 #include "parson.h"
 
-#define TABLE_SIZE 2599999
+#define TABLE_SIZE 5000000
 #define NAME_SIZE 100
-#define DATA_SIZE 256
 
 typedef struct {
 	int key;
 	char name[NAME_SIZE];
 	int phone_number;
-	int lateNumber;
+	int late_number;
 } element;
 
 struct list {
@@ -26,6 +25,7 @@ int hash_function(int key) {
 	return key /* % TABLE_SIZE*/;
 }
 
+// 해시 데이터 추가
 void hash_chain_add(element item, struct list* ht[]) {
 	int hash_value = hash_function(item.key);
 	struct list* ptr;
@@ -41,7 +41,7 @@ void hash_chain_add(element item, struct list* ht[]) {
 	ptr->item.key = item.key;
 	strcpy(ptr->item.name, item.name);
 	ptr->item.phone_number = item.phone_number;
-	ptr->item.lateNumber = item.lateNumber;
+	ptr->item.late_number = item.late_number;
 
 	ptr->link = NULL;
 
@@ -61,12 +61,13 @@ void hash_chain_search(int key) {
 		return;
 	}
 	for (node = hash_table[hash_value]; node; node = node->link) {
-		printf("%d, %s, 0%d %d", node->item.key, node->item.name, node->item.phone_number, node->item.lateNumber);
+		printf("%d, %s, 0%d %d", node->item.key, node->item.name, node->item.phone_number, node->item.late_number);
 	}
 	printf("\n");
 	printf("=======================================\n");
 }
 
+// 해시 데이터 수정
 void hash_chain_change(int key, int change_value) {
 	struct list* node;
 	int hash_value = hash_function(key);
@@ -75,10 +76,11 @@ void hash_chain_change(int key, int change_value) {
 		return;
 	}
 	node = hash_table[hash_value];
-	node->item.lateNumber = change_value;
+	node->item.late_number = change_value;
 
 }
 
+// JSON 파일 읽어와서 해시에 저장
 void json_file_reading() {
 	element data;
 
@@ -99,8 +101,8 @@ void json_file_reading() {
 			data.phone_number = (int)json_object_get_number(item, "phoneNumber");
 			printf("전화번호 : %d\n", data.phone_number);
 
-			data.lateNumber = json_object_get_number(item, "dueDateOverCheck");
-			printf("연체 여부 : %d\n", data.lateNumber);
+			data.late_number = (int)json_object_get_number(item, "dueDateOverNumber");
+			printf("연체 횟수 : %d\n", data.late_number);
 
 			hash_chain_add(data, hash_table);
 			printf("\n");
@@ -109,7 +111,54 @@ void json_file_reading() {
 	json_value_free(root_Value);
 }
 
-void json_file_writing(int student_num, int late_check) {
+size_t json_file_find_index(int student_num, JSON_Array* find_data) {
+	size_t array_size = json_array_get_count(find_data);
+	for (size_t i = 0; i < array_size; i++) {
+		JSON_Object* item = json_array_get_object(find_data, i);
+		if (item != NULL && ((int)json_object_get_number(item, "studentNumber")) == student_num) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+// 블랙리스트 추가
+void json_blacklist_add(int student_num) {
+	// 연체 횟수 3회 초과할 시 3달 제한
+	int borrow_limit_day = 3;
+
+	JSON_Value* root_Value = json_parse_file("blackList.json");
+	JSON_Object* root_Object = json_value_get_object(root_Value);
+
+	JSON_Array* data_Array = json_object_get_array(root_Object, "blackListInfo");
+	size_t array_size = json_array_get_count(data_Array);
+
+	JSON_Value* blackList_data = json_value_init_object();
+	JSON_Object* blackList_object = json_value_get_object(blackList_data);
+
+	json_object_set_number(blackList_object, "studentNumber", student_num);
+	json_object_set_number(blackList_object, "borrowLimitDay", borrow_limit_day);
+
+	json_array_append_value(data_Array, blackList_data);
+	json_serialize_to_file_pretty(root_Value, "blackList.json");
+	json_value_free(root_Value);
+}
+
+void json_blacklist_delete(int student_num) {
+	JSON_Value* root_Value = json_parse_file("blackList.json");
+	JSON_Object* root_Object = json_value_get_object(root_Value);
+
+	JSON_Array* black_Array = json_object_get_array(root_Object, "blackListInfo");
+	size_t index = json_file_find_index(student_num, black_Array);
+
+	json_array_remove(black_Array, index);
+
+	json_serialize_to_file_pretty(root_Value, "blackList.json");
+	json_value_free(root_Value);
+}
+
+// JSON 파일 업데이트
+void json_file_rewriting(int student_num) {
 
 	JSON_Value* root_Value = json_parse_file("studentData.json");
 	JSON_Object* root_Object = json_value_get_object(root_Value);
@@ -117,14 +166,101 @@ void json_file_writing(int student_num, int late_check) {
 	JSON_Array* data_Array = json_object_get_array(root_Object, "studentInfo");
 	size_t array_size = json_array_get_count(data_Array);
 
-	JSON_Object* change = json_array_get_object(data_Array, 0);
-	if (change != NULL) {
-		int change_value = json_object_get_number(change, "dueDateOverCheck");
+	size_t index = json_file_find_index(student_num, data_Array);
+	int change_value = 0;
+
+	JSON_Object* change = json_array_get_object(data_Array, index);
+	int check_limit = (int)json_object_get_number(change, "dueDateOverCheck");
+
+	if (change != NULL && check_limit != 1) {
+		change_value = (int)json_object_get_number(change, "dueDateOverNumber");
 		change_value += 1;
-		json_object_set_number(change, "dueDateOverCheck", change_value);
+		json_object_set_number(change, "dueDateOverNumber", change_value);
 		hash_chain_change(student_num, change_value);
 	}
+	if (change_value >= 3 && check_limit != 1) {
+		json_blacklist_add(student_num);
+		json_object_set_number(change, "dueDateOverCheck", 1);
+		json_object_set_number(change, "dueDateOverNumber", 0);
+	}
+	else if (check_limit == 1) {
+		json_blacklist_delete(student_num);
+		json_object_set_number(change, "dueDateOverCheck", 0);
+		hash_chain_change(student_num, 0);
+	}
 	json_serialize_to_file_pretty(root_Value, "studentData.json"); // Json 파일 변경점 저장, 이 코드가 있어야 JSON파일이 수정됨
+	json_value_free(root_Value);
+}
+
+void insert_string_data(char* buffer, int size) {
+	fgets(buffer, size, stdin);
+	buffer[strcspn(buffer, "\n")] = '\0';
+}
+
+void json_data_add() {
+
+	char newName[20] = "";
+	int student_num = 0;
+	int phone_num = 0;
+	int due_date_over_num = 0;
+	int borrow_limit = 0;
+
+
+	printf("학번 : ");
+	scanf_s("%d", &student_num);
+	while (getchar() != '\n');
+
+	printf("이름 : ");
+	insert_string_data(newName, 20);
+
+	printf("전화번호 : ");
+	scanf_s("%d", &phone_num);
+	while (getchar() != '\n');
+
+	JSON_Value* root_Value = json_parse_file("studentData.json");
+	JSON_Object* root_Object = json_value_get_object(root_Value);
+
+	JSON_Array* data_Array = json_object_get_array(root_Object, "studentInfo");
+	size_t array_size = json_array_get_count(data_Array);
+
+	JSON_Value* new_student_data = json_value_init_object();
+	JSON_Object* new_student_object = json_value_get_object(new_student_data);
+
+	json_object_set_number(new_student_object, "studentNumber", student_num);
+	json_object_set_string(new_student_object, "name", newName);
+	json_object_set_number(new_student_object, "phoneNumber", phone_num);
+	json_object_set_number(new_student_object, "dueDateOverNumber", due_date_over_num);
+	json_object_set_number(new_student_object, "dueDateOverCheck", borrow_limit);
+
+	json_array_append_value(data_Array, new_student_data);
+	json_serialize_to_file_pretty(root_Value, "studentData.json");
+	json_value_free(root_Value);
+
+}
+
+void check_borrow_limit() {
+	JSON_Value* root_Value = json_parse_file("blackList.json");
+	JSON_Object* root_Object = json_value_get_object(root_Value);
+
+	JSON_Array* data_Array = json_object_get_array(root_Object, "blackListInfo");
+	size_t array_size = json_array_get_count(data_Array);
+	for (size_t i = 0; i < array_size; i++) {
+		JSON_Object* change = json_array_get_object(data_Array, i);
+		int student_num = (int)json_object_get_number(change, "studentNumber");
+		if (change != NULL) {
+			int limit_day = (int)json_object_get_number(change, "borrowLimitDay");
+			if (limit_day == 0) {
+				json_file_rewriting(student_num);
+			}
+			else if (limit_day > 0) {
+				int limit_day = (int)json_object_get_number(change, "borrowLimitDay");
+				json_object_set_number(change, "borrowLimitDay", limit_day - 1);
+				hash_chain_change(student_num, limit_day);
+				json_serialize_to_file_pretty(root_Value, "blackList.json"); // Json 파일 변경점 저장, 이 코드가 있어야 JSON파일이 수정됨
+			}
+		}
+
+	}
 	json_value_free(root_Value);
 }
 
@@ -132,10 +268,12 @@ int main() {
 	int choice;
 	int student_num;
 
+
 	json_file_reading();
 
 	while (1) {
-		printf("선택지 : 1. 학생조회 2. 종료 3. (미완성기능)파일 업데이트=> ");
+		check_borrow_limit();
+		printf("선택지 : 1. 학생조회 2. 종료 3. 연체 학생 업데이트 4. 새로운 학생 추가=> ");
 		scanf_s("%d", &choice);
 		if (choice == 1) {
 			printf("학번 입력 => ");
@@ -147,12 +285,14 @@ int main() {
 		else if (choice == 3) {
 			printf("학번 입력 => ");
 			scanf_s("%d", &student_num);
-			json_file_writing(student_num, 0);
+			json_file_rewriting(student_num);
 			hash_chain_search(student_num);
+		}
+		else if (choice == 4) {
+			json_data_add();
 		}
 		else
 			printf("다시 입력하십시오\n\n");
 	}
-	json_file_reading();
 	return 0;
 }
